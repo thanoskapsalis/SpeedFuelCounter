a81820202d11e830cf110874d3196cee03f04340
Thanoskap <thanoskapsalis97@gmail.com>
2019-10-14 21:34:59 +0300

    semi ready

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,50 @@ public class Main {
     public static void main(String[] args) throws Throwable {
         Scanner keyboard = new Scanner(System.in);
         Trip trip = null;
-        System.out.println("1)Start a Trip\n2)Stop a trip\n3)Total Trip Kilometers\n4)Add Trip Kilometers ");
-        int choise;
+        System.out.println("1)Start a Trip\n2)Stop a trip\n3)Total Trip Kilometers\n4)Add Trip Kilometers\n5)Set Consumption ");
+        int choice = 100;
         do {
-            System.out.print("Choose an option: ");
-            choise = keyboard.nextInt();
+            try {
+                System.out.print("Choose an option: ");
+                choice = keyboard.nextInt();
 
-            switch (choise) {
-                case (1):
-                    trip = new Trip();
-                    System.out.println("New Trip Stared!");
-                    break;
-                case (2):
+                switch (choice) {
+                    case (1):
+                        trip = new Trip();
+                        System.out.println("New Trip Stared!");
+                        break;
+                    case (2):
 
-                    trip.EmptyTrip();
-                    System.out.println("Trip Deleted");
-                    break;
-                case (3):
-                    try {
+                        trip.EmptyTrip();
+                        trip = null;
+                        System.out.println("Trip Deleted");
+                        break;
+                    case (3):
+
                         System.out.println("Total kilometetes: " + trip.getKilometers());
-                    } catch (NullPointerException e) {
-                        System.out.println("No trip has been initialized so far");
-                    }
-                    break;
+
+                        break;
+                    case (4):
+                        System.out.print("Kilometers driven: ");
+                        System.out.println();
+                        int kilometers = keyboard.nextInt();
+                        trip.AddKilometers(kilometers);
+                        break;
+                    case (5):
+                        System.out.print("Consumption: ");
+                        System.out.println();
+                        float consumption = keyboard.nextFloat();
+                        trip.setConsumption(consumption);
 
 
+                }
+
+
+            } catch (NullPointerException e) {
+                System.out.println("No trip has been initialized so far ");
             }
 
-        } while (choise != 0);
+        } while (choice != 0);
 
 
     }
--- src/Trip.java
+++ src/Trip.java
@@ -2,45 +2,37 @@ import java.util.ArrayList;
 
 public class Trip {
 
-    private static ArrayList<Integer> kilometers = new ArrayList<>();
-    private static ArrayList<Float> consumption = new ArrayList<>();
+    private int kilometers;
+    private float consumption ;
 
 
-    public void AddKilometers(Odometer odometer) {
-        kilometers.add(odometer.getKilometers());
-        consumption.add(odometer.getConsumption());
-        Calculate();
+    public void AddKilometers(int kilometers) {
+        this.kilometers=this.kilometers+kilometers;
+        System.out.println("Kilometers Drivern " + kilometers);
+
+    }
+
+    public Trip()
+    {
+        kilometers=0;
+        consumption=0;
+    }
+
+    public void setConsumption(float consumption)
+    {
+        this.consumption=consumption;
     }
 
     //Καθαρίζει τους πίνακες και αποδεσμεύει το παρον ταξίδι απο την μνήμη
     public void EmptyTrip() throws Throwable {
 
-        kilometers.clear();
-        consumption.clear();
+        kilometers=0;
+        consumption=0;
         this.finalize();
     }
 
 
-
-    private void Calculate() {
-        int maxKilometers = 0;
-        float maxConsumption = 0;
-        for (int i = 0; i < kilometers.size(); i++)
-            maxKilometers = maxKilometers + kilometers.get(i);
-        for (int i = 0; i < consumption.size(); i++)
-            maxConsumption = maxConsumption + consumption.get(i);
-        System.out.println("Kilometers Drivern " + maxKilometers);
-        System.out.println("Maximum Concumption " + maxConsumption);
-    }
-
-    public int getKilometers() {
-        int maxKilometers = 0;
-        if(kilometers.isEmpty())
-            return 0;
-        for (int i = 0; i < kilometers.size(); i++)
-            maxKilometers = maxKilometers + kilometers.get(i);
-        return maxKilometers;
-    }
+    public int getKilometers(){return kilometers;}
 
 
 }
